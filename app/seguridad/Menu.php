43a09f30e8a518a446d568b9b1178b8991ba0d87
javimontoyaq93 <javimontoyaq93@gmail.com>
2017-04-27 00:11:31 -0500

    Inicio

---
+++ app/seguridad/Menu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\\seguridad;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    //
+}
---
+++ app/seguridad/Permiso.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\\seguridad;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permiso extends Model
+{
+    //
+}
