dbe723de01e58131e55503447a004de6d2883cfb
javimontoyaq93 <javimontoyaq93@gmail.com>
2017-04-27 01:02:43 -0500

    Relacion de modelos del modulo de seguridad

--- app/seguridad/GrupoUsuario.php
+++ app/seguridad/GrupoUsuario.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\\seguridad;
+namespace App\\Seguridad;
 
 use Illuminate\Database\Eloquent\Model;
 
 class GrupoUsuario extends Model
 {
-    //
-}
+    protected $fillable = ['nombre', 'descripcion', 'eliminado'];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
